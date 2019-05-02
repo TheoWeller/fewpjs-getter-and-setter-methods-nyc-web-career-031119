264bc0ae2614d28061d6e2a2aef872846bdf60be
Theo Weller <weller.theodor@gmail.com>
2019-05-02 22:02:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your Circle class here
+class Circle{
+  constructor(radius){
+    this.radius = radius;
+  }
+
+  get diameter(){
+    return this.radius * 2;
+  }
+
+  get circumference(){
+    let pie = Math.PI
+    return 2 * pie * this.radius;
+  }
+}
